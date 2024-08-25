477ea3654fd38f54d4896cd28477d893cbca3d9f
Alex Chapman <alex.d.chapman@outlook.com>
2024-08-25 10:21:46 +0000

    [FIX] Adjusting code so that it takes input as stdin

--- Day_1/P1.cpp
+++ Day_1/P1.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
 #include <fstream>
-#include <string>
+#include <iostream>
 #include <sstream>
+#include <string>
 
 using namespace std;
 
@@ -9,34 +9,29 @@ using namespace std;
 string puzzle_line;
 string alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-int main()
-{
+int main() {
+    cout << "---Start---\n";
     // Read from the text file
-    ifstream MyReadFile("Day_1/input");
     int total = 0;
 
-    // Use a while loop together with the getline() function to read the file line by line
-    while (getline(MyReadFile, puzzle_line))
-    {
-
+    // Use a while loop together with the getline() function to read the file
+    // line by line
+    string puzzle_line;
+    while (cin >> puzzle_line) {
         string value;
         int int_value;
 
         // Output the text from the file
-        for (int x = 0; x <= puzzle_line.length(); x++)
-        {
-            if (alphabet.find(puzzle_line[x]) == std::string::npos)
-            {
+        for (int x = 0; x <= puzzle_line.length(); x++) {
+            if (alphabet.find(puzzle_line[x]) == std::string::npos) {
                 int first_value = puzzle_line[x] - '0';
                 value = puzzle_line[x];
                 break;
             }
         }
 
-        for (int x = puzzle_line.length() - 1; x >= 0; x--)
-        {
-            if (alphabet.find(puzzle_line[x]) == std::string::npos)
-            {
+        for (int x = puzzle_line.length() - 1; x >= 0; x--) {
+            if (alphabet.find(puzzle_line[x]) == std::string::npos) {
                 int last_value = puzzle_line[x] - '0';
                 value += puzzle_line[x];
                 break;
@@ -48,6 +43,5 @@ int main()
     }
     cout << total;
 
-    // Close the file
-    MyReadFile.close();
+    cout << "\n----End----\n";
 }
